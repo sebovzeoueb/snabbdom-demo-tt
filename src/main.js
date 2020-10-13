fa6aa0006275ed37857445d8b094e93129a88f77
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2020-10-13 11:48:00 +0200

    various counter functions

--- src/main.js
+++ src/main.js
@@ -14,28 +14,56 @@ const patch = init([
   attributesModule
 ])
 
-let state = {
+const buttonValues = [4, 20, 55]
 
+let state = {
+  counter: 0,
+  pressedKeys: []
 }
 
 let vnode = document.body
 
-const view = (state, send) => h('body', [
-  h('h1', 'Snabbdom Demo')
-])
+const view = (state, send) => h('body', 
+  [
+    h('h1', 'Snabbdom Demo'),
+    h('div#buttons', [
+      h('div.button', {on: {click: e => send({type: 'increment'})}}, 'Increment'),
+      h('div.button', {on: {click: e => send({type: 'decrement'})}}, 'Decrement'),
+      ...buttonValues.map(v => h('div.button', {on: {click: e => send({type: 'set', value: v})}}, `Set to ${v}`))
+    ]),
+    h('div#readouts', [
+      h('div.readout', state.counter),
+      state.pressedKeys.length ? h('div.readout', `You are pressing ${state.pressedKeys.join(', ')}`) : undefined
+    ])
+  ]
+)
 
 const effect = (state, action, send) => {
-
+  if (action.type == 'init') {
+    window.onkeydown = e => send({type: 'keydown', key: e.key, repeat: e.repeat})
+    window.onkeyup = e => send({type: 'keyup', key: e.key})
+    window.onblur = e => send({type: 'blur'})
+  }
+  if (action.type == 'keydown') {
+    if (action.key == '+') send({type: 'increment'})
+    if (action.key == '-') send({type: 'decrement'})
+  }
 }
 
 const update = (state, action) => {
+  if (action.type == 'increment') state.counter++
+  if (action.type == 'decrement') state.counter--
+  if (action.type == 'set') state.counter = action.value
+  if (action.type == 'keydown') if (!action.repeat) state.pressedKeys.push(action.key)
+  if (action.type == 'keyup') state.pressedKeys.splice(state.pressedKeys.indexOf(action.key), 1)
+  if (action.type == 'blur') state.pressedKeys = []
   return state
 }
 
 const send = action => {
   state = update(state, action)
   vnode = patch(vnode, view(state, send))
-  effect(state,action,send) 
+  effect(state, action, send) 
 }
 
 send({type: 'init'})
